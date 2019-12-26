2e91847a828f3be7893aafedea32772d35cc5edb
jake-thsong <jake.thsong@kakaocorp.com>
2019-12-26 09:44:48 +0900

    change -> clcick.prevent로 변경

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -3,39 +3,42 @@
     <input
         type="checkbox"
         :checked="done"
-        @change="onChange"
+        @click.prevent="onChange"
     >
-    <span :class="{'is-done': done }">{{text}}</span>
+    <span :class="{'is-done': done}">{{text}}</span>
     <button @click="deleteClick">X</button>
   </li>
 </template>
 
 <script>
   export default {
-    name: "TodoListItem",
-    props: ['itemData'],
-    computed: {
-      done() {
-        return this.itemData.done;
-      },
-      id() {
-        return this.itemData.id;
-      },
-      text() {
-        return this.itemData.text;
+    props:['itemData'],
+    data() {
+      return {
+        done : this.itemData.done,
+        id : this.itemData.id,
+        text : this.itemData.text
+      }
+    },
+    watch: {
+      itemData(newVal) {
+        this.$nextTick()
+          .then(() => {
+            this.done = newVal.done;
+          })
       }
     },
     methods: {
       deleteClick() {
         this.$emit('delete', this.id);
       },
-      onChange() {
-        this.$emit('update', {id: this.id, value: !this.done});
+      onChange(e) {
+        this.$emit('update', {id:this.id, value: !this.done});
       }
     }
   }
 </script>
 
-<style scoped>
+<style>
 
 </style>
